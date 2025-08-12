74ff8a0d7bd3fd78fe07855bec52db8961d04e9c
TotevM <mtotev53@gmail.com>
2025-08-12 18:52:22 +0300

    Add React Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import './App.css';
+import {Routes, Route } from 'react-router';
+import Header from './components/Header';
 
 function App() {
+    return (
+        <body>
+            <div id='box'>
+                <Header />
 
-  return (
-    <></>
-  )
+                <main id='main-content'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                    </Routes>
+                </main>
+            </div>
+        </body>
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/components/GameCreate.jsx
@@ -0,0 +1,46 @@
+export default function GameCreate() {
+    return (
+        <section id='create-page' className='auth'>
+            <form id='create'>
+                <div className='container'>
+                    <h1>Create Game</h1>
+                    <label htmlFor='leg-title'>Legendary title:</label>
+                    <input
+                        type='text'
+                        id='title'
+                        name='title'
+                        placeholder='Enter game title...'></input>
+
+                    <label htmlFor='category'>Category:</label>
+                    <input
+                        type='text'
+                        id='category'
+                        name='category'
+                        placeholder='Enter game category...'></input>
+
+                    <label htmlFor='levels'>MaxLevel:</label>
+                    <input
+                        type='number'
+                        id='maxLevel'
+                        name='maxLevel'
+                        min='1'
+                        placeholder='1'></input>
+
+                    <label htmlFor='game-img'>Image:</label>
+                    <input
+                        type='text'
+                        id='imageUrl'
+                        name='imageUrl'
+                        placeholder='Upload a photo...'></input>
+
+                    <label htmlFor='summary'>Summary:</label>
+                    <textarea name='summary' id='summary'></textarea>
+                    <input
+                        className='btn submit'
+                        type='submit'
+                        value='Create Game'></input>
+                </div>
+            </form>
+        </section>
+    );
+}
