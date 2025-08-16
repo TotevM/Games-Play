9f2b319fb4208ab619b0e3cec19b5c4d3bb7db8f
TotevM <mtotev53@gmail.com>
2025-08-16 22:45:33 +0300

    Display Games List On Home Page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
 import heroImg from '../images/four_slider_img01.png';
 
-
 export default function Home() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/games')
+            .then((response) => response.json())
+            .then((res) => setGames(Object.values(res)));
+    }, []);
+
     return (
         <section id='welcome-world'>
             <div className='welcome-message'>
@@ -11,28 +19,53 @@ export default function Home() {
             <img src={heroImg} alt='hero'></img>
 
             <div id='home-page'>
-                <h1>Latest Games</h1>
+                {games.length !== 0 ? (
+                    games.slice(0,3).map((game) => (
+                        <div className='game' key={game._id}>
+                            <div className='image-wrap'>
+                                <img src={game.imageUrl}></img>
+                            </div>
+                            <h3>{game.title}</h3>
+                            <div className='rating'>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div className='data-buttons'>
+                                <a href='#' className='btn details-btn'>
+                                    Details
+                                </a>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className='no-articles'>No games yet</p>
+                )}
+                {/* <h1>Latest Games</h1>
+                {games.map((game) => (
+                    <div className='game' key={game._id}>
+                        <div className='image-wrap'>
+                            <img src={game.imageUrl}></img>
+                        </div>
+                        <h3>{game.title}</h3>
+                        <div className='rating'>
+                            <span>☆</span>
+                            <span>☆</span>
+                            <span>☆</span>
+                            <span>☆</span>
+                            <span>☆</span>
+                        </div>
+                        <div className='data-buttons'>
+                            <a href='#' className='btn details-btn'>
+                                Details
+                            </a>
+                        </div>
+                    </div>
+                ))}
 
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='../images/CoverFire.png'></img>
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
-
-                <p className='no-articles'>No games yet</p>
+                <p className='no-articles'>No games yet</p> */}
             </div>
         </section>
     );
