d37edc3326971b31cc7edf84afc279672f33b6c7
TotevM <mtotev53@gmail.com>
2025-09-04 09:21:29 +0300

    Create AuthGuard.jsx

---
+++ client/src/guards/AuthGuard.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router';
+import { Navigate } from 'react-router';
+import { useAuth } from '../hooks/useAuth';
+
+export default function AuthGuard() {
+    const isAuthenticated = useAuth();
+    if (!isAuthenticated) {
+        return <Navigate to='/login' replace />;
+    }
+
+    return Outlet;
+}
